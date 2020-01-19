62324accffec7b79384d86a9ccf81cd447544482
YurgenUA <yuri@print.com>
2020-01-19 23:51:23 +0200

    first implementation

---
+++ setup-static.tfvars
@@ -0,0 +1,2 @@
+acmcertificatearn = "to be filled"
+route53-zone-id = "to be filled"
--- worker.py
+++ worker.py
@@ -1,30 +1,56 @@
 import os
 import time
-
+import re
+from pythonping import ping
 
 def overwrite_check(absfilename):
     if os.path.exists(absfilename):
-        raise Exception("File {absfilename} already exists.")
+        raise Exception(f'File {absfilename} already exists.')
 
+def ping_address(url):
+    try:
+        response_list = ping(url)
+        return response_list.success()
+    except Exception:
+        return False
+
+def parse_destination(destination, variables):
+    pattern = re.compile(r'\/\/(.*)\..*\.(.*)\..*\..*\/(.*)\/')
+    matches = pattern.findall(destination)
+    if len(matches) != 1 or len(matches[0]) !=3 :
+        error = f'Failed to match {destination} with expected format.'
+        raise Exception(error)
+
+    variables['api-id'] = matches[0][0]
+    variables['region'] = matches[0][1]
+    variables['stage'] = matches[0][2]
 
 def attach(host, subdomain, destination):
     response = {}
     response['status'] = 'INITIALIZING'
-    # TODO: check if subdomain exists in DNS.  If yes, raise Exception
+    
+    variables = {'host': host, 'subdomain': subdomain}
+    parse_destination(destination, variables)
+
+    if ping_address(f'{subdomain}.{host}'):
+        error = f'Subdomain {subdomain}.{host} already exists.'
+        raise Exception(error)
+
     # TODO: ping destination, raise EXCEPTION if non-200
-    # TODO: use boto3 to query AWS for any important details such as VPC information
-    # TODO: Use command line interface to ask user questions, if necessary
+    # API Gateway doesn't allow ping, so to check I suggest to use AWS SDK (boto3). Probably same can be done for subdomain 
+
     fn = 'setup.tfvars'
     overwrite_check(fn)
-    tfvars = "write all variables to it (e.g. subdomain, VPC, etc)"
+    tfvars = ""
+    for k, v in variables.items():
+        tfvars += f'{k} = "{v}"\r\n'
     f = open(fn, 'w')
     f.write(tfvars)
     f.close()
-    # TODO: check if `setup.tf` exists in the current location.  If so, raise Exception
-    # TODO: generate Terraform recipe and save it as `setup.tf`
-    # TODO: Terraform recipe uses Route53 and other services to  connect subdomain to destination
-    # TODO: Run `terraform plan` via something like `os.command`.  Capture results to `terraform.plan.txt`
+    
     ts = int(time.time() * 1000)
     response['tf_plan'] = f'{ts}.plan.txt'
-    response['status'] = 'NOT IMPLEMENTED (soon: SUCCESS)'
+    os.system(f'terraform plan -var-file="setup-static.tfvars" -var-file="setup.tfvars" -out {ts}.plan.txt')
+    
+    response['status'] = 'NOT TESTED (soon: SUCCESS)'
     return response
